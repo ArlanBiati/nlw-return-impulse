ce7d949ee4bb5674d3907a331fe8bfba65e54c97
Arlan Gustavo Biati <arlan@MacBook-Air-de-Arlan.local>
2022-05-09 21:09:43 -0300

    :sparkles: finalização backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-const test = 1;
+import express from 'express';
+import cors from 'cors';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log('HTTP server running!');
+});
